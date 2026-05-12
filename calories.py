f9d840e938ecef32b760ed6d3ff9dfbe6e80b69b
Myname <assa123assa1234@gmail.com>
2026-05-12 19:28:50 +0400

    калькулятор калорий

--- calories.py
+++ calories.py
@@ -1,1 +1,12 @@
-print("test")
+print("Привет я калькулятор калорий!")
+print("Пожалуйста введи необходимые данные :")
+object = input("что ты сьел?")
+weight = float(input("Вес продукта?"))
+calories_per_100g = float(input("сколько калорий на 100г?"))
+calories = (weight/100)*calories_per_100g
+if calories >= 2000:
+    print("Осторожно это много")
+elif calories <= 500: 
+    print("Можно сьесть что нибудь еще")
+
+else : print("Приятного аппетита")
